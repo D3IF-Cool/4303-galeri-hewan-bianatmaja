570db52869c4dee85fa6fce42f171ec480258d8e
Bian Taufikurrahman Atmaja <bianatmaja@student.telkomuniversity.ac.id>
2021-05-04 20:50:38 +0700

    Membuat view model untuk fitur main

---
+++ app/src/main/java/org/d3if0021/galerihewan/ui/main/MainFragment.kt
@@ -0,0 +1,45 @@
+package org.d3if0021.galerihewan.ui.main
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.observe
+import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.RecyclerView
+import org.d3if0021.galerihewan.databinding.FragmentMainBinding
+
+class MainFragment : Fragment() {
+
+    private val viewModel: MainViewModel by lazy {
+        ViewModelProvider(this).get(MainViewModel::class.java)
+    }
+    private lateinit var binding: FragmentMainBinding
+    private lateinit var myAdapter: MainAdapter
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentMainBinding.inflate(layoutInflater, container, false)
+        myAdapter = MainAdapter()
+        with(binding.recyclerView) {
+            addItemDecoration(
+                DividerItemDecoration(context, RecyclerView.VERTICAL)
+            )
+            adapter = myAdapter
+            setHasFixedSize(true)
+        }
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        viewModel.getData().observe(viewLifecycleOwner, {
+            myAdapter.updateData(it)
+        })
+    }
+}
