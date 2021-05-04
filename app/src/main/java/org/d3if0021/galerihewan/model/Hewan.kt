a30f664c2500e910a419f89b160131140fef6391
Bian Taufikurrahman Atmaja <bianatmaja@student.telkomuniversity.ac.id>
2021-05-04 21:05:02 +0700

    Memproses data internet sebagai JSON

--- app/src/main/java/org/d3if0021/galerihewan/model/Hewan.kt
+++ app/src/main/java/org/d3if0021/galerihewan/model/Hewan.kt
@@ -1,7 +1,10 @@
 package org.d3if0021.galerihewan.model
 
+import org.d3if0021.galerihewan.R
+
 data class Hewan (
     val nama: String,
     val namaLatin: String,
-    val imageResId: Int
+    val imageResId: Int = R.drawable.angsa
+
 )
